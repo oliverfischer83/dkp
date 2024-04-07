b2621c97e3a56d1c86c3c261bc5831013af62046
Oliver Fischer <oliver.fischer3@bosch.com>
2024-04-07 13:50:22 +0200

    .

--- src/dkp/pages/02_Info.py
+++ src/dkp/pages/02_Info.py
@@ -4,16 +4,18 @@ import streamlit as st
 def main():
 
     st.set_page_config(
-        page_title='DKP - Info',
-        page_icon='💰️',  # see https://twemoji-cheatsheet.vercel.app/
+        page_title="DKP - Info",
+        page_icon="💰️",  # see https://twemoji-cheatsheet.vercel.app/
         layout="wide",
-        initial_sidebar_state="expanded")
+        initial_sidebar_state="expanded",
+    )
 
     st.subheader("Regeln")
-    st.markdown("""
+    st.markdown(
+        """
     - Guthaben ...
-      - +100 DKP als Startguthaben
-      - +50 DKP pro Raidtag bei Raidende gutgeschrieben
+      - `+100 DKP` als Startguthaben
+      - `+50 DKP` pro Raidtag bei Raidende gutgeschrieben
       - darf nicht überzogen werden
       - ist Personen-gebunden
       - wird selbstständig kontrolliert
@@ -22,21 +24,28 @@ def main():
       - bei Gleichstand entscheiden die Würfel
       - nur auf Items eigener Rüstungsklasse oder Rezepte bieten
       - Random Loot wird nicht verteilt
-    """)
+    """
+    )
 
     st.subheader("Loot Addon")
-    st.markdown("""
+    st.markdown(
+        """
     - das Addon zum Verteilen des Raid-Loots heißt [RCLootCouncil](https://rclootcouncil.com/)
-    - da wir keine Raid-Gilde sind, muss in den Addon Einstellungen (`/rc config`) die Option `???Gildenraids???` deaktiviert werden
-    """)
+    - da wir keine Raid-Gilde sind, muss in den Addon Einstellungen (`/rc config`) die Option `Guild Groups Only` deaktiviert werden
+    """
+    )
+    st.image("media/rclootcouncil_config.png", width=600)
 
     st.subheader("Wieso DKP?")
-    st.markdown("""
+    st.markdown(
+        """
     - DKP steht für Dragon Kill Points und ist ein System zur `gerechten Verteilung` von Loot in Raids.
     - Spieler erhalten die `Möglichkeit` sich ihr `Wunschitem` in gewisser Weise durch Raidteilnahme `zu erarbeiten`
     - die reine `Raidteilnahme wird belohnt`, unabhängig davon, ob Bosse gelegt wurden oder nicht
     - das Bieten auf Items und das Verwalten der eignen DKP bringt mehr \"Würze\" in den Raidalltag
-    """)
+    """
+    )
+
 
 # entry point
 main()
